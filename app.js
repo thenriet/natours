f9aa13361c580b45f57153032929a5842160410b
thenriet <dabalx@gmail.com>
2023-06-27 17:25:23 +0200

    POST endpoint now operational

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 
 const app = express();
 
+// This is a middleware, a function that can modifiy the incoming data
+// It is needed to get access to the req.body from the POST request
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -21,6 +25,28 @@ app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours: tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  //Get the the id from the last object from the db and add 1 to it
+  const newId = tours[tours.length - 1].id + 1;
+  // Use Object.assign to merge 2 objects into a new one
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
