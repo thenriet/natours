9e05f9609aabc456909a0013ee719bf9fe20fdd9
thenriet <dabalx@gmail.com>
2023-06-28 10:16:13 +0200

    refactoring

--- app.js
+++ app.js
@@ -7,27 +7,29 @@ const app = express();
 // It is needed to get access to the req.body from the POST request
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours: tours } });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+  // callback function used with the find function will return
+  // the only element that match req.params (so the id present in the url)
+  // JS trick : multiply a string that looks like a number will automatically convert it to a number
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({ status: 'failed', message: 'invalid id' });
+  }
+  res.status(200).json({ status: 'success', data: { tour: tour } });
+};
+
+const createTour = (req, res) => {
   //   console.log(req.body);
   //Get the the id from the last object from the db and add 1 to it
   const newId = tours[tours.length - 1].id + 1;
@@ -47,10 +49,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-// doesnt really update anything, this is just to demonstrate the logic
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const id = req.params.id * 1;
   if (id > tours.length) {
     return res.status(404).json({ status: 'failed', message: 'invalid Id' });
@@ -61,25 +62,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here>',
     },
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-
-  // callback function used with the find function will return
-  // the only element that match req.params (so the id present in the url)
-  // JS trick : multiply a string that looks like a number will automatically convert it to a number
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  if (!tour) {
-    return res.status(404).json({ status: 'failed', message: 'invalid Id' });
-  }
-  res.status(200).json({ status: 'success', data: { tour: tour } });
-});
-
-// doesnt really delete anything, this is just to demonstrate the logic
-
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   const id = req.params.id * 1;
   if (id > tours.length) {
     return res.status(404).json({ status: 'failed', message: 'invalid Id' });
@@ -88,7 +73,22 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// // doesnt really update anything, this is just to demonstrate the logic
+// app.patch('/api/v1/tours/:id', updateTour);
+// // doesnt really delete anything, this is just to demonstrate the logic
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
