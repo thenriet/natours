51af6a636693b0bc0f1f7271891f1392bf19a9a5
thenriet <dabalx@gmail.com>
2023-07-10 14:57:54 +0200

    local database connection with mongodb and mongoose ready

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection established');
+  });
 // environment variables made by express.js
 console.log(app.get('env'));
 // environment variables made by node.js
