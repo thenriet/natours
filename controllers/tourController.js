e5cbfe1110b654f0318634eb52917838c1d722f6
thenriet <dabalx@gmail.com>
2023-07-10 16:22:17 +0200

    createTour function redone, using async/await and the TourSchema from mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,15 +13,17 @@ const Tour = require('./../models/tourModel');
 //   next();
 // };
 
-exports.checkBody = (req, res, next) => {
-  console.log('Check body is called');
-  if (!req.body.name || !req.body.price) {
-    return res
-      .status(400)
-      .json({ status: 'failed', message: 'Missing name or price' });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   console.log('Check body is called');
+//   if (!req.body.name || !req.body.price) {
+//     return res
+//       .status(400)
+//       .json({ status: 'failed', message: 'Missing name or price' });
+//   }
+//   next();
+// };
+
+//////////////////////////////// MVC ARCHITECTURE STARTS HERE //////////////////////////////////
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -43,26 +45,39 @@ exports.getTour = (req, res) => {
   // res.status(200).json({ status: 'success', data: { tour: tour } });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
   //   console.log(req.body);
   //Get the the id from the last object from the db and add 1 to it
   // const newId = tours[tours.length - 1].id + 1;
   // Use Object.assign to merge 2 objects into a new one
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    // JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        // data: {
-        //   tour: newTour,
-        // },
-      });
-    }
-  );
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // tours.push(newTour);
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/tours-simple.json`,
+  //   // JSON.stringify(tours),
+  //   (err) => {
+  //     res.status(201).json({
+  //       status: 'success',
+  //       // data: {
+  //       //   tour: newTour,
+  //       // },
+  //     });
+  //   }
+  // );
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
